62df8a7e4dd8a77ad2704f106f55ac7a8e35cc14
Theo Vesque <theovesque171201@gmail.com>
2023-10-28 23:21:39 +0200

    Début de la page d'accueil

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Théo Vesque - Portfolio</title>
+  <link rel="stylesheet" href="/src/assets/css/index.css">
+</head>
+
+<body>
+  <h1 id="titre">Théo Vesque</h1>
+  <div id=textBio_CV>
+    <p id="text_bio">Bonjour, je m'appelle Théo Vesque et je suis actuellement étudiant en 3ème année de BUT Informatique à l'UPHF. Je suis passionné par le développement web et je suis à la recherche d'un stage d'une durée de 4 mois pour le mois d'Avril. Mon objectif est d'acquérir une expérience pratique et de contribuer activement à des projets innovants. Je vous invite à explorez mon portfolio si vous souhaitez en savoir plus sur mes compétences et mes divers projets réalisés. </p>
+    <a href="/src/assets/images/background.jpg" target="_blank">Consulter mon CV</a>
+  </div>
+  <?php
+  include 'includes/header.html';
+  ?>
+</body>
+
+</html>
